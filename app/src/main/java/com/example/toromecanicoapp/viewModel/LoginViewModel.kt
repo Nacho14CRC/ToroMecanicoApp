6915029d6fc01a15c9ddd8fc2ed6f6d3029376da
Nacho14CRC <nachoinacurso@gmail.com>
2023-11-21 14:39:59 -0600

    Firebase

--- app/src/main/java/com/example/toromecanicoapp/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/toromecanicoapp/viewModel/LoginViewModel.kt
@@ -1,18 +1,28 @@
 package com.example.toromecanicoapp.viewModel
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import com.example.toromecanicoapp.state.LoginState
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import java.util.Date
 
 class LoginViewModel : ViewModel() {
 	private val _uiState = MutableStateFlow(LoginState())
+	private val auth: FirebaseAuth = Firebase.auth
+	private  val _loading = MutableLiveData(false)
+
 	val uiState: StateFlow<LoginState> = _uiState.asStateFlow()
 	var sUsuario by mutableStateOf("")
 		private set
@@ -55,7 +65,20 @@ class LoginViewModel : ViewModel() {
 		sConfirmarContrasena= sNuevoValor
 	}
 	
-	fun Login() {
+	fun Login(email:String, password:String) {
+		Log.d("Login","Entró")
+		try {
+			auth.signInWithEmailAndPassword(email, password)
+				.addOnCompleteListener { task ->
+					if (task.isSuccessful){
+						Log.d("Firebase", "signInWithEmailAndPassword logueado")
+					} else {
+						Log.d("Firebase", "signInWithEmailAndPassword: ${task.result.toString()}")
+					}
+				}
+		} catch (ex: Exception){
+			Log.d("Firebase", "signInWithEmailAndPassword: ${ex.message}")
+		}
 	
 	}
 	fun CrearCuenta() {
