1ad89aac7f2d9f027497ad2ffbed3b36db04238d
Andrei Arguedas Corrales <andreiac@hotmail.es>
2023-11-21 16:02:13 -0600

    Redireccionamiento 50%

--- app/src/main/java/com/example/toromecanicoapp/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/example/toromecanicoapp/screens/login/LoginScreenViewModel.kt
@@ -1,27 +1,34 @@
 package com.example.toromecanicoapp.screens.login
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
 
 class LoginScreenViewModel : ViewModel() {
 	private val auth: FirebaseAuth = Firebase.auth
-	fun Login(email:String, password:String) {
-		Log.d("Login","Entró")
+	private val _loading = MutableLiveData(false)
+	fun Login(email:String, password:String, home: ()-> Unit)
+			= viewModelScope.launch {
+		
 		try {
 			auth.signInWithEmailAndPassword(email, password)
 				.addOnCompleteListener { task ->
 					if (task.isSuccessful){
-						Log.d("Firebase", "signInWithEmailAndPassword logueado")
+						Log.d("toroMecanicoApp", "Login: logueado")
+						home()
+						
 					} else {
-						Log.d("Firebase", "Error : ${task.result.toString()}")
+						Log.d("toroMecanicoApp", "Login Error : ${task.result.toString()}")
 					}
 				}
 		} catch (ex: Exception){
-			Log.d("Firebase", "signInWithEmailAndPassword: ${ex.message}")
+			Log.d("toroMecanicoApp", "Login Catch: ${ex.message}")
 		}
 	
 	}
